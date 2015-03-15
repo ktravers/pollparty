eba00c01fc189607b30b615d6081f0e8c8b64907
ktravers <travers.kt@gmail.com>
2015-03-15 19:56:44 -0400

    update config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pollparty_session'
+Rails.application.config.session_store :cookie_store, key: '_whosaheadnow_session'
